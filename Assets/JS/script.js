d103d3c4b7f2e102fd96b412e0bd2b183404e7f9
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-06 15:15:56 -0500

    added an onclick function to the search button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,4 +6,39 @@ $(document).ready(function() {
     var searchCity = "";
     // APIKEY FOR OPENWEATHERMAP API
     var APIKey = "ff190912e73db2be11befe0e9575b205";
+
+    // FUNCTIONALITY FOR THE SEARCH BUTTON
+    $("#searchBtn").on("click", function() {
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // END OF JS FILE
