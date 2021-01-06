6e07a58c3540f46a8719f554c893901f8bdb8d5f
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-06 14:49:37 -0500

    added APIKey unprotected, the key isn't important in this project

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,5 +4,6 @@
 $(document).ready(function() {
     // INPUT FIELD IS BLANK SO USER CAN ENTER THE CITY THEY WANT A FORECAST FOR
     var searchCity = "";
-
+    // APIKEY FOR OPENWEATHERMAP API
+    var APIKey = "ff190912e73db2be11befe0e9575b205";
 // END OF JS FILE
