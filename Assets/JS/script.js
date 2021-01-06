06ba787562fcaa29eb33c57a187d4964c70233f5
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-06 14:49:06 -0500

    started variable for searching the city the user wants to find weather for

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,2 +1,8 @@
 // BEGINNING OF JS FILE
+
+
+$(document).ready(function() {
+    // INPUT FIELD IS BLANK SO USER CAN ENTER THE CITY THEY WANT A FORECAST FOR
+    var searchCity = "";
+
 // END OF JS FILE
