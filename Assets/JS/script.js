1bc6fb55e1667b82ab0d5ffdf9e6259c51d975b9
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-06 19:03:05 -0500

    will now build out functions for future development

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,27 +1,36 @@
 // BEGINNING OF JS FILE
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     // INPUT FIELD IS BLANK SO USER CAN ENTER THE CITY THEY WANT A FORECAST FOR
     var searchCity = "";
     // APIKEY FOR OPENWEATHERMAP API
     var APIKey = "ff190912e73db2be11befe0e9575b205";
 
     // FUNCTIONALITY FOR THE SEARCH BUTTON
-    $("#searchBtn").on("click", function() {
+    $("#searchBtn").on("click", function () {
         if (saveCityList()) {
             retrieveWeather(true);
         };
     });
 
-    $(document).on("click","td", function(e){
+    $(document).on("click", "td", function (e) {
         inputCity = e.target.innerHTML;
         saveLastCitySearched(inputCity);
         retrieveWeather(false);
     });
 
+    //THIS FUNCTION IS USED TO RETRIEVE THE WEATHER.  THE 'NEEDCITY' ARGUMENT IS USED
+    //TO DETERMINE IF THE CITY HAS BEEN INPUT BY THE USER
+    function retrieveWeather(needCity) {
+        if (needCity) {
+            getInputCity()
+        }
+        //OR IF THE CITY IS BEING RETRIEVED FROM LOCAL STORAGE, OR FROM LIST OF PREVIOUSLY SEARCHED CITIES
+        buildTodaysWeather();
+        buildFiveDayForecast();
+    };
 
-   
 
 
 });
