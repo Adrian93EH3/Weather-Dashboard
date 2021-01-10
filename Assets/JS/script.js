468329c9e4095ab2a3da5791716cf8880fe05622
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-09 20:07:35 -0500

    moved the inputCity function so that it wouldn't be hoisted and instead can look more fluid

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,14 +6,25 @@ $(document).ready(function () {
     var searchCity = "";
     // APIKEY FOR OPENWEATHERMAP API
     var APIKey = "ff190912e73db2be11befe0e9575b205";
-
+    
     // FUNCTIONALITY FOR THE SEARCH BUTTON
     $("#searchBtn").on("click", function () {
         if (saveCityList()) {
             retrieveWeather(true);
         };
     });
-
+    
+    //GET THE CITY THAT THE USER INPUT. ALSO, ALERT USER IF THERE IS NOTHING IN THE INPUT FIELD
+    function getInputCity(){
+        inputCity = $("#searchInput").val().trim();
+        if (inputCity == "") {
+            alert("Please enter a city to search for.")
+            return false;
+        }
+        return true;
+        
+    }
+    
     $(document).on("click", "td", function (e) {
         inputCity = e.target.innerHTML;
         saveLastCitySearched(inputCity);
@@ -133,16 +144,6 @@ $(document).ready(function () {
 
 
 
-    //GET THE CITY THAT THE USER INPUT. ALSO, ALERT USER IF THERE IS NOTHING IN THE INPUT FIELD
-    function getInputCity(){
-        inputCity = $("#searchInput").val().trim();
-        if (inputCity == "") {
-            alert("Please enter a city to search for.")
-            return false;
-        }
-        return true;
-        
-    }
 
 });
 
