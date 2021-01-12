0a18ba2e2dcb57a6eadc7cd85269d148a5aaf0cc
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-11 21:32:23 -0500

    intiliaze document

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
     // APIKEY FOR OPENWEATHERMAP API
     var APIKey = "ff190912e73db2be11befe0e9575b205";
 
+    
+
     // FUNCTIONALITY FOR THE SEARCH BUTTON
     $("#searchBtn").on("click", function () {
         if (saveCityList()) {
@@ -164,6 +166,14 @@ $(document).ready(function () {
         }
 
     }
+
+    function initDoc() {
+        retrievePreviouslySearchedList();
+        inputCity = retrieveLastCitySearched();
+        if (inputCity != null) {
+            retrieveWeather(false);
+        }
+    };
 
 
 
