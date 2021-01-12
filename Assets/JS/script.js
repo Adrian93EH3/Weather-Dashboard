48609f94830e658dbf05dce5d08d43c1308e0518
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-11 21:34:30 -0500

    put the function of initdoc in use

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     // APIKEY FOR OPENWEATHERMAP API
     var APIKey = "ff190912e73db2be11befe0e9575b205";
 
-
+    initDoc();
 
     // FUNCTIONALITY FOR THE SEARCH BUTTON
     $("#searchBtn").on("click", function () {
